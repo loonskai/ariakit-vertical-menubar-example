cf0106a65202bac20eb9724a7e58006e15e69ea6
Siarhei Lunski <slunski@box.com>
2025-01-08 15:31:09 +0100

    Init

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import Example from "./menubar-navigation/index.tsx";
+
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(<StrictMode><Example /></StrictMode>);
+}
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  // @ts-ignore
+  plugins: [react()],
+});
